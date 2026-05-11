afd716d98156d23da0c75c0377fe863defcbfb25
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-05-11 23:55:10 +0900

    repository 생성

--- src/main/java/com/site/sub/Answer.java
+++ src/main/java/com/site/sub/Answer.java
@@ -1,10 +1,14 @@
 package com.site.sub;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
 public class Answer {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/site/sub/Question.java
+++ src/main/java/com/site/sub/Question.java
@@ -1,11 +1,15 @@
 package com.site.sub;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
 public class Question {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
