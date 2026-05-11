3854c3fb5d88d3c3a788acb8fc45b96d97398689
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-05-12 00:03:31 +0900

    test 작성

--- src/test/java/com/site/sub/SubApplicationTests.java
+++ src/test/java/com/site/sub/SubApplicationTests.java
@@ -1,13 +1,30 @@
 package com.site.sub;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ActiveProfiles("test")
 @SpringBootTest
 class SubApplicationTests {
+	@Autowired
+	private QuestionRepository questionRepository;
 
 	@Test
-	void contextLoads() {
+	@DisplayName("findAll")
+	void t1() {
+		List<Question> questions = questionRepository.findAll();
+
+		assertEquals(2, questions.size());
+
+		Question q = questions.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
 	}
 
 }
