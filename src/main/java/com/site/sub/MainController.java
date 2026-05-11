892fdda9e0e6d737a9813b70cf7510c0f3b4f160
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-05-11 22:30:01 +0900

    블라우저 index 표시

---
+++ src/main/java/com/site/sub/MainController.java
@@ -0,0 +1,16 @@
+package com.site.sub;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+	//url과 매핑된 메서드는 결과값을 리턴해야한다.
+	@GetMapping("/sbb")
+	@ResponseBody //URL 요청에 대한 응답으로 문자열을 리턴한다는 뜻
+	public String index() {
+		return "index";
+	}
+
+}
