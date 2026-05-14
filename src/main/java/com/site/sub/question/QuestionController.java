f43391546593d1f1c7edcec977454c13e01fb478
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-05-14 17:07:43 +0900

    상세 페이지 생성

--- src/main/java/com/site/sub/question/QuestionController.java
+++ src/main/java/com/site/sub/question/QuestionController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -48,5 +49,12 @@ public class QuestionController {
 		List<Question> questionList = this.questioniService.getList();
 		model.addAttribute("questionList" , questionList);
 		return "question_list";
+	}
+
+	@GetMapping(value = "/question/detail/{id}")
+	public String detail(Model model, @PathVariable("id") Integer id) {
+		Question question = questioniService.getQuestioin(id);
+		model.addAttribute("question", question);
+		return "question_detail";
 	}
 }
--- src/main/java/com/site/sub/question/QuestioniService.java
+++ src/main/java/com/site/sub/question/QuestioniService.java
@@ -1,9 +1,11 @@
 package com.site.sub.question;
 
+import com.site.sub.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor //생성자 주입
@@ -12,5 +14,15 @@ public class QuestioniService {
 
 	public List<Question> getList() {
 		return questionRepository.findAll();
+	}
+
+	public Question getQuestioin(int id) {
+		Optional<Question> opQuestion = questionRepository.findById(id);
+
+		if (opQuestion.isPresent()) {
+			return opQuestion.get();
+		} else {
+			throw new DataNotFoundException("question not found");
+		}
 	}
 }
